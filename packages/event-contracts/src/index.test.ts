dd5eaf6b222b14e61eb692377c2b08072ae01643
acam49 <abdullaheta1@gmail.com>
2026-07-27 16:30:31 +0300

    fix(ci): resolve gitleaks false positive in event contracts test

--- packages/event-contracts/src/index.test.ts
+++ packages/event-contracts/src/index.test.ts
@@ -16,7 +16,7 @@ test("validateNormalizedMovementEvent parses token transfer event", () => {
     blockNumber: 19000000,
     fromAddress: "0xSenderAddress",
     toAddress: "0xReceiverAddress",
-    tokenAddress: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+    tokenAddress: "0x0000000000000000000000000000000000000001",
     tokenSymbol: "USDT",
     rawAmount: "1000000000",
     decimalAmount: 1000,
